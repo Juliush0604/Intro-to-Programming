571dc8d747b24f55107a2fd9ffb48cea722574fb
gersteinj <gersteinj@gmail.com>
2017-11-02 02:05:07 -0400

    added exercise 2

---
+++ exercise2.py
@@ -0,0 +1,17 @@
+# lots of animals
+my_list = ['aardvark', 'bat', 'cat', 'dog', 'elephant', 'frog', 'gorilla', 'hippopotamus', 'iguana', 'jellyfish', 'koala', 'lamprey', 'moose', 'newt', 'octopus', 'panda', 'quail', 'rabbit', 'snail', 'tapir', 'unicorn', 'vulture', 'wasp', 'x-ray tetra', 'yak', 'zebra']
+
+# print out the item at index 1. Before you run this, do you know what that item is?
+print(my_list[1])
+
+# Insert code similar to what I have on line 5 that will print the fifth item in the list, elephant
+
+
+# Do the same for gorilla, which is the seventh item
+
+
+# Let's try working backwards from the end
+# This will print the last item in the list, zebra
+print(my_list[-1])
+
+# Use similar code to print yak, which is second from the end, and unicorn, which is sixth from the end
