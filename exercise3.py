dae243f80f56848edd82716f693f25b9b19ff5f7
Julien <jheurteux@ucvts.tec.nj.us>
2017-11-07 17:26:43 +0000

    Finished exercise 3

--- exercise3.py
+++ exercise3.py
@@ -5,6 +5,7 @@ my_list = ['aardvark', 'bat', 'cat', 'dog', 'elephant', 'frog', 'gorilla', 'hipp
 mini_list = my_list[1:4]
 
 # What will be printed? Include it in a comment right below this
+#'bat', 'cat', 'dog'
 print(mini_list)
 
 # printing a smaller list containing the first five items
@@ -13,14 +14,16 @@ print(my_list[:5])
 # Modify the line of code below so that it prints out a list
 # consisting of the section from cat through iguana,
 # including both of those terms
-print(my_list[:])
+print(my_list[2:9])
 
 # Do the same for rabbit through viper
-print(my_list[:])
+#Do you mean vulture?
+print(my_list[17:22])
 
 # What happens if you use a slice that is blank for start and end?
 # Try it out below
-
+print(my_list[:])
 
 # How about if you use the same index for both start and end?
 # Try it out below
+print(my_list[5:5])
